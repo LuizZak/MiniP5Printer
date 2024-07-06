dfcc2ea2b30cb51cddd0c9934befb3450cf67d5d
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-07-06 11:25:52 -0300

    Changing minimum swift-tools-version to 5.9

--- Package.swift
+++ Package.swift
@@ -1,23 +1,21 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "MiniP5Printer",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MiniP5Printer",
-            targets: ["MiniP5Printer"]),
+            targets: ["MiniP5Printer"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MiniP5Printer"),
+            name: "MiniP5Printer"
+        ),
         .testTarget(
             name: "MiniP5PrinterTests",
-            dependencies: ["MiniP5Printer"]),
+            dependencies: ["MiniP5Printer"]
+        ),
     ]
 )
